662aaa26df0bec5a893c2d5aaa239fad9b8667f2
Jonatan <jonatan.kilhamn@gmail.com>
2014-10-26 11:50:36 +0100

    Set up and started running roadData_setup for sizes 1-2

--- my_code/roadData_setup.m
+++ my_code/roadData_setup.m
@@ -4,7 +4,7 @@ sizes = [100, 200, 500, 1000, 2000, 5000, 10000, 20000];
 nGraphs = 100;
 
 
-for graphSize = sizes(3:end);
+for graphSize = sizes(1:2);
     CreateRoadData(graphSize, nGraphs)
 end
 
--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
-/home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay -r "fprintf(1,'Hej');exit;"
-cat experiment_setup.m
+/home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/fake_experiment.m >& my_code/logs/fake_experiment.log
