01bb4b349b893db7b74c3f864b687e2f8595e0ac
Jonatan <jonatan.kilhamn@gmail.com>
2014-11-14 16:16:31 +0100

    Tried splitting up fwcombined even more--one section per run--to avoid memory issues

--- my_code/roadData_fwcombined.m
+++ my_code/roadData_fwcombined.m
@@ -5,6 +5,7 @@ sizes = [100, 200, 500, 1000, 2000, 5000, 10000, 20000];
 sizesToRun = sizes(6);
 
 noOfSections = 8;
+currentSection = 1;
 
 for graphSize = sizesToRun
     
@@ -14,6 +15,8 @@ for graphSize = sizesToRun
     % we now have ROADS and lroads loaded
     nGraphs = size(ROADS, 2);
     
+    
+    % Use these two rows for the first section:
     fwCombinedROADS = cell(1, nGraphs);
     fwCombinedRuntimesROADS = zeros(1, nGraphs);
     
@@ -22,9 +25,12 @@ for graphSize = sizesToRun
     fwRuntimeFilename = ...
         ['./my_code/data/fwRuntime_ROADS' ...
         num2str(graphSize)];
+    % Use these rows for the other sections:
+    %load(fwFilename);
+    %load(fwRuntimeFilename);
     
     
-    for section = 1:noOfSections
+    for section = currentSection
         fwSectionFilename = ['./my_code/data/fw_ROADS' ...
             num2str(graphSize) '-' num2str(section)];
         fwSectionRuntimeFilename = ...
@@ -40,19 +46,20 @@ for graphSize = sizesToRun
         stopInd = floor(section*(nGraphs/noOfSections));
         for i=startInd:stopInd
             fwCombinedROADS{i} = fwROADS{i};
-            fwCombinedRuntimesROADS(i) = fwRuntimesROADS(i);            
+            fwCombinedRuntimesROADS(i) = fwRuntimesROADS(i);
         end
         
         clear('fwRuntimesROADS');
         clear('fwROADS');
         
+
+        save(fwFilename, 'fwCombinedROADS', '-v7.3');
+        save(fwRuntimeFilename, 'fwCombinedRuntimesROADS', '-v7.3');
+        
     end
     
-    fwROADS = fwCombinedROADS;
-    fwRuntimesROADS = fwCombinedRuntimesROADS;
     
-    save(fwFilename, 'fwROADS', '-v7.3');
-    save(fwRuntimeFilename, 'fwRuntimesROADS', '-v7.3');
+    
     
     
 end
