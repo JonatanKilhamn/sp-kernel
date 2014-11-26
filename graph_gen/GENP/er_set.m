f6d1c0a730607c0ba2d497564df426ce2d9fb136
Jonatan <jonatan.kilhamn@gmail.com>
2014-11-26 14:04:08 +0100

    Added the GENP files from Fredrik

---
+++ graph_gen/GENP/er_set.m
@@ -0,0 +1,35 @@
+function [graphs, labels] = er_set(N,n,p1,p2)
+
+labels = [zeros(N,1); ones(N,1)];
+
+ams = cell(2*N,1); als = cell(2*N,1); nls = cell(2*N,1);
+for i=1:N
+    A = erdosRenyi(n,p1);
+    
+    al = cell(n,1);
+    for j=1:n
+        al{j} = find(A(j,:));
+    end
+    nl= struct();
+    nl.values = ones(n,1);
+    
+    ams{i} = A;
+    als{i} = al;
+    nls{i} = nl;
+    
+    A = erdosRenyi(n,p2);
+    
+    al = cell(n,1);
+    for j=1:n
+        al{j} = find(A(j,:));
+    end
+    nl= struct();
+    nl.values = ones(n,1);
+    
+    ams{N+i} = A;
+    als{N+i} = al;
+    nls{N+i} = nl;
+end
+
+graphs = struct('am',ams,'al',als,'nl',nls);
+
