ef19d44d1e95fcd5c4d0dcecbac77ee8202990fc
Jonatan <jonatan.kilhamn@gmail.com>
2014-11-27 14:02:52 +0100

    Some new scripts

--- vor_pre15.sh
+++ vor_pre15.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
 cd sp-kernel
-nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/roadData_vorPre.m > my_code/logs/roadData_vorPre15.log 2>&1
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_vorPre.m > my_code/logs/genP_vorPre15.log 2>&1
