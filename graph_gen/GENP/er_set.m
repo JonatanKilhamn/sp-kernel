4cb658b8354d50550df1ce285d8c19bb142ef862
Jonatan <jonatan.kilhamn@gmail.com>
2014-11-27 13:27:24 +0100

    Included check for disconnected graphs in er_set

--- graph_gen/GENP/er_set.m
+++ graph_gen/GENP/er_set.m
@@ -4,7 +4,12 @@ labels = [zeros(N,1); ones(N,1)];
 
 ams = cell(2*N,1); als = cell(2*N,1); nls = cell(2*N,1);
 for i=1:N
+    
     A = erdosRenyi(n,p1);
+    while any(all(~A,2))
+        A = erdosRenyi(n,p1);
+    end
+
     
     al = cell(n,1);
     for j=1:n
@@ -18,6 +23,9 @@ for i=1:N
     nls{i} = nl;
     
     A = erdosRenyi(n,p2);
+    while any(all(~A,2))
+        A = erdosRenyi(n,p2);
+    end
     
     al = cell(n,1);
     for j=1:n
@@ -31,6 +39,7 @@ for i=1:N
     nls{N+i} = nl;
     disp(['Done with step ', num2str(i), ' out of ', num2str(N)])
 end
+
 
 graphs = struct('am',ams,'al',als,'nl',nls);
 
