1d315d71b269480aa7f28241efafec47d15d65d6
Jonatan <jonatan.kilhamn@gmail.com>
2014-11-28 09:27:37 +0100

    Actual combined ErrAcc data for sizes 1--4

---
+++ erracccombined14.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
+cd sp-kernel
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_erracccombined.m > my_code/logs/genP_erracccombined14.log 2>&1
