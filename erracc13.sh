2cbf9ae827812729e13b0a7d191ac0e9c23c19f6
Jonatan <jonatan.kilhamn@gmail.com>
2014-12-01 13:27:33 +0100

    Scripts and results from erracc13 run

--- erracc13.sh
+++ erracc13.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
 cd sp-kernel
-nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_erracc.m > my_code/logs/genP_erracc13.log 2>&1
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_erracc.m > my_code/logs/genP_erracc13_vor.log 2>&1
