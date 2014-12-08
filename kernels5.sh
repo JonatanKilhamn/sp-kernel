44169f9b25f23c1a1ccd3bd7b9fed0e7241c3638
Jonatan <jonatan.kilhamn@gmail.com>
2014-12-08 14:51:21 +0100

    New scripts for new runs

--- kernels5.sh
+++ kernels5.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
 cd sp-kernel
-nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_kernels.m > my_code/logs/genP_kernels5.log 2>1
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_kernels.m > my_code/logs/proto_kernels5_vor.log 2>1
--- setup_genp.sh
+++ setup_genp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
 cd sp-kernel
-nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/genP_setup.m > my_code/logs/genP_setup7.log 2>&1
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/genP_setup.m > my_code/logs/genP_setup16.log 2>&1
