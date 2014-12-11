75206b50e33f92d7c27067f8dd6d59088fb5ec2d
Jonatan <jonatan.kilhamn@gmail.com>
2014-12-11 09:20:14 +0100

    New scripts for new runs

---
+++ erracc16.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
+cd sp-kernel
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/run_erracc.m > my_code/logs/proto_erracc16.log 2>&1
