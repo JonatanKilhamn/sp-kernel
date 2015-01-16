94679f34b00896eef3e066b5e893312687cca5f1
Jonatan <jonatan.kilhamn@gmail.com>
2015-01-16 13:01:12 +0100

    New scripts

--- run2_proto.sh
+++ run2_proto.sh
@@ -2,11 +2,15 @@
 export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
 #cd sp-kernel
 
-nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay -r "experiment_setup; run_erracccombined('PROTO',0); exit" > my_code/logs/proto_erracccombined0.log 2>&1
+ZERO=0
+#qsub proto_erracc.sh $ZERO
+#qsub proto_runtimes.sh $ZERO
 
 
 for SIZE in $(cat proto_sizes); do
 qsub proto_erracc.sh $SIZE
-qsub proto_disterr.sh $SIZE
+#qsub proto_disterr.sh $SIZE
+qsub proto_runtimes.sh $SIZE
+
 done
 
