740f431b7a5c9da64475f0f32e5116e876c2b378
Jonatan <jonatan.kilhamn@gmail.com>
2015-02-04 09:27:02 +0100

    New DD, now without disconnected graphs

---
+++ DD_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LD_LIBRARY_PATH="/home/jonkil/lib:$LD_LIBRARY_PATH"
+cd sp-kernel
+nohup /home/jonkil/MATLAB/bin/matlab -nojvm -nodisplay < my_code/dd_setup.m > my_code/logs/DD_setup.log 2>&1
